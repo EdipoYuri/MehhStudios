9d766ad063d7ed4ca5e68ce9c9936da434daa4d8
Édipo Yuri Ribeiro da Silva <edipoyurirs@gmail.com>
2018-09-06 19:52:13 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+      navigation: true,
+      navigationPosition: 'right',
+      //navigationTooltips: ['Section 1', 'Section 2','Section 3','Section 4',],
+      showActiveTooltip: true,
+      slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+      controlArrows:false,
+    });
+});
