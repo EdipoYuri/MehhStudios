344aa6fda9e598b2fd446bd40fb41cda13894962
Édipo Yuri Ribeiro da Silva <edipoyurirs@gmail.com>
2018-09-09 10:55:16 -0300

    Background animado

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#fullpage').fullpage({
-      navigation: true,
-      navigationPosition: 'right',
-      //navigationTooltips: ['Section 1', 'Section 2','Section 3','Section 4',],
-      showActiveTooltip: true,
-      slidesNavigation: true,
+        navigation: true,
+        navigationPosition: 'right',
+        //navigationTooltips: ['Section 1', 'Section 2','Section 3','Section 4',],
+        showActiveTooltip: true,
+        slidesNavigation: true,
         slidesNavPosition: 'bottom',
-      controlArrows:false,
+        controlArrows: false,
     });
 });
+
+/*window.addEventListener('scroll', function(){
+    console.log('entrou');
+    document.getElementById("fullpage").classList.remove('blue');
+    document.getElementById("fullpage").classList.add('red');
+})*/
